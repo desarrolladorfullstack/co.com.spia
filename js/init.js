aae11c9ddb9b19d22f2672e04aa407ed3b5dc9f3
DESARROLLADOR FULLSTACK <desarrolladorweb@spia.co>
2022-05-21 09:44:46 -0500

    revert js video playing
    20220521

--- js/init.js
+++ js/init.js
@@ -624,18 +624,6 @@ $(".glitch").mgGlitch({
 function arlo_tm_videoplayer(){
 	"use strict";
 	$(".youtube-bg").mb_YTPlayer();
-	let video = document.body.querySelector('video');
-	if ( video !=  null) {
-		video.click();
-		video.play().then(val=>{
-			/*document.body.querySelector('.arlo_tm_hero .background').style.display="none";*/
-			console.log("video played", val);
-			setTimeout(()=>{
-				/*document.body.querySelector('.arlo_tm_hero .background').style.display="block";*/
-				arlo_tm_videoplayer();
-			}, 10000)
-		})
-	}
 }
 
 // -----------------------------------------------------
